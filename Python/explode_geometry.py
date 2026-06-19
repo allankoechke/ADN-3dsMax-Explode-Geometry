effb9d974b66d63ec9d271282d3b33f0aeaa199a
Allan K. Koech <allankoech.ke@gmail.com>
2026-06-19 18:26:01 +0300

    feat: migrate Python/explode_geometry.py from print() to logging module

--- Python/explode_geometry.py
+++ Python/explode_geometry.py
@@ -9,8 +9,25 @@ from PySide6 import QtCore, QtWidgets
 import pymxs
 import qtmax
 import random
+import logging
+import sys
 
 rt = pymxs.runtime
+
+logger = logging.getLogger("ExplodeGeometry")
+if not logger.handlers:
+    logger.setLevel(logging.WARNING)
+
+    _stdout_handler = logging.StreamHandler(sys.stdout)
+    _stdout_handler.setLevel(logging.DEBUG)
+    _stdout_handler.addFilter(lambda r: r.levelno < logging.WARNING)
+    _stdout_handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(_stdout_handler)
+
+    _stderr_handler = logging.StreamHandler(sys.stderr)
+    _stderr_handler.setLevel(logging.WARNING)
+    _stderr_handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(_stderr_handler)
 
 class _GCProtector(object):
     widgets = []
@@ -153,46 +170,46 @@ class Form(QtWidgets.QWidget, Ui_Form):
         deleteOriginal = self.delete_original_checkbox.isChecked()
         mode = "TriMesh" if self.trimesh_option.isChecked() else "MNMesh"
 
-        print("[ExplodeGeometry] --- Explode triggered ---")
-        print("[ExplodeGeometry] Mode:            {}".format(mode))
-        print("[ExplodeGeometry] Add Shell:        {} (offset={})".format(addShell, shell_amount))
-        print("[ExplodeGeometry] Add Edit Mesh:    {}".format(addEditMesh))
-        print("[ExplodeGeometry] Collapse Stack:   {}".format(collapseNode))
-        print("[ExplodeGeometry] Center Pivot:     {}".format(centerPivot))
-        print("[ExplodeGeometry] Delete Original:  {}".format(deleteOriginal))
-        print("[ExplodeGeometry] Selected objects: {}".format(len(rt.selection)))
+        logger.debug("[ExplodeGeometry] --- Explode triggered ---")
+        logger.debug("[ExplodeGeometry] Mode:            %s", mode)
+        logger.debug("[ExplodeGeometry] Add Shell:        %s (offset=%s)", addShell, shell_amount)
+        logger.debug("[ExplodeGeometry] Add Edit Mesh:    %s", addEditMesh)
+        logger.debug("[ExplodeGeometry] Collapse Stack:   %s", collapseNode)
+        logger.debug("[ExplodeGeometry] Center Pivot:     %s", centerPivot)
+        logger.debug("[ExplodeGeometry] Delete Original:  %s", deleteOriginal)
+        logger.debug("[ExplodeGeometry] Selected objects: %d", len(rt.selection))
 
         if len(rt.selection) <= 0:
             msg = "Nothing selected. Please select a node to explode."
-            print("[ExplodeGeometry] ERROR: {}".format(msg))
+            logger.warning("[ExplodeGeometry] ERROR: %s", msg)
             self.show_alert(msg)
         else:
             with pymxs.undo(True, "Explode Geometry"):
                 if self.trimesh_option.isChecked():
                     transformation_set = list(rt.selection)
-                    print("[ExplodeGeometry] [TriMesh] Processing {} object(s)".format(len(transformation_set)))
+                    logger.debug("[ExplodeGeometry] [TriMesh] Processing %d object(s)", len(transformation_set))
                     for i, n in enumerate(transformation_set, 1):
-                        print("[ExplodeGeometry] [TriMesh] Object {}/{}: '{}'".format(i, len(transformation_set), n.name))
+                        logger.debug("[ExplodeGeometry] [TriMesh] Object %d/%d: '%s'", i, len(transformation_set), n.name)
                         convert_to_triangle_faces(n, addShell, shell_amount,
                                                   addEditMesh, collapseNode,
                                                   centerPivot)
                         if deleteOriginal:
-                            print("[ExplodeGeometry] [TriMesh] Deleting original '{}'".format(n.name))
+                            logger.debug("[ExplodeGeometry] [TriMesh] Deleting original '%s'", n.name)
                             rt.delete(n)
 
                 if self.mnmesh_option.isChecked():
                     transformation_set = list(rt.selection)
-                    print("[ExplodeGeometry] [MNMesh] Processing {} object(s)".format(len(transformation_set)))
+                    logger.debug("[ExplodeGeometry] [MNMesh] Processing %d object(s)", len(transformation_set))
                     for i, node in enumerate(transformation_set, 1):
-                        print("[ExplodeGeometry] [MNMesh] Object {}/{}: '{}'".format(i, len(transformation_set), node.name))
+                        logger.debug("[ExplodeGeometry] [MNMesh] Object %d/%d: '%s'", i, len(transformation_set), node.name)
                         convert_to_mnmesh_faces(node, addShell, shell_amount,
                                                 addEditMesh, collapseNode,
                                                 centerPivot)
                         if deleteOriginal:
-                            print("[ExplodeGeometry] [MNMesh] Deleting original '{}'".format(node.name))
+                            logger.debug("[ExplodeGeometry] [MNMesh] Deleting original '%s'", node.name)
                             rt.delete(node)
 
-            print("[ExplodeGeometry] Done. Redrawing views.")
+            logger.info("[ExplodeGeometry] Done. Redrawing views.")
             rt.redrawViews()
             rt.callbacks.removeScripts(id=rt.Name('explodeGeom'))
             self.close()
@@ -214,73 +231,73 @@ def _random_wire_color():
 
 
 def convert_to_triangle_faces(node, addShell, shell_amount, addEditMesh, collapseNode, centerPivot):
-    print("[ExplodeGeometry] [TriMesh] Converting '{}' to mesh...".format(node.name))
+    logger.debug("[ExplodeGeometry] [TriMesh] Converting '%s' to mesh...", node.name)
     rt.convertToMesh(node)
     # After convertToMesh the stack is collapsed and getVert returns world-space positions
     num_faces = rt.getNumFaces(node)
-    print("[ExplodeGeometry] [TriMesh] '{}' has {} face(s) to explode".format(node.name, num_faces))
+    logger.debug("[ExplodeGeometry] [TriMesh] '%s' has %d face(s) to explode", node.name, num_faces)
     for face_idx in range(1, num_faces + 1):
         face = rt.getFace(node, face_idx)
         v1 = rt.getVert(node, int(face.x))
         v2 = rt.getVert(node, int(face.y))
         v3 = rt.getVert(node, int(face.z))
-        print("[ExplodeGeometry] [TriMesh] Face {}/{}: verts=({:.2f},{:.2f},{:.2f}) ({:.2f},{:.2f},{:.2f}) ({:.2f},{:.2f},{:.2f})".format(
-            face_idx, num_faces, v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z))
+        logger.debug("[ExplodeGeometry] [TriMesh] Face %d/%d: verts=(%.2f,%.2f,%.2f) (%.2f,%.2f,%.2f) (%.2f,%.2f,%.2f)",
+            face_idx, num_faces, v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z)
 
         new_node = rt.mesh(vertices=rt.Array(v1, v2, v3),
                            faces=rt.Array(rt.Point3(1, 2, 3)))
         rt.update(new_node)
         new_node.wireColor = _random_wire_color()
-        print("[ExplodeGeometry] [TriMesh] Created piece '{}' for face {}".format(new_node.name, face_idx))
+        logger.debug("[ExplodeGeometry] [TriMesh] Created piece '%s' for face %d", new_node.name, face_idx)
 
         applySettings(new_node, addShell, shell_amount,
                       addEditMesh, collapseNode, centerPivot)
-    print("[ExplodeGeometry] [TriMesh] Finished exploding '{}'".format(node.name))
+    logger.debug("[ExplodeGeometry] [TriMesh] Finished exploding '%s'", node.name)
 
 
 def convert_to_mnmesh_faces(node, addShell, shell_amount, addEditMesh, collapseNode, centerPivot):
-    print("[ExplodeGeometry] [Poly] Converting '{}' to poly...".format(node.name))
+    logger.debug("[ExplodeGeometry] [Poly] Converting '%s' to poly...", node.name)
     rt.convertToPoly(node)
     num_faces = rt.polyop.getNumFaces(node)
-    print("[ExplodeGeometry] [Poly] '{}' has {} face(s) to explode".format(node.name, num_faces))
+    logger.debug("[ExplodeGeometry] [Poly] '%s' has %d face(s) to explode", node.name, num_faces)
 
     # Detach faces in reverse order so earlier face indices stay valid as later ones are removed.
     # polyop.detachFaces returns a boolean (not the new INode), so we snapshot existing node
     # handles before each detach and find the newly added node by handle exclusion.
     for face_idx in range(num_faces, 0, -1):
-        print("[ExplodeGeometry] [Poly] Detaching face {}/{}".format(face_idx, num_faces))
+        logger.debug("[ExplodeGeometry] [Poly] Detaching face %d/%d", face_idx, num_faces)
         rt.polyop.detachFaces(node, rt.Array(face_idx), asNode=True) # True/False
         new_node = rt.objects[-1]
         # print (new_node) # Check if the node is valid
         if new_node is None:
-            print("[ExplodeGeometry] [Poly] WARNING: Could not find detached node for face {}".format(face_idx))
+            logger.warning("[ExplodeGeometry] [Poly] WARNING: Could not find detached node for face %d", face_idx)
             continue
         new_node.wireColor = _random_wire_color()
-        print("[ExplodeGeometry] [Poly] Created piece '{}' for face {}".format(new_node.name, face_idx))
+        logger.debug("[ExplodeGeometry] [Poly] Created piece '%s' for face %d", new_node.name, face_idx)
         applySettings(new_node, addShell, shell_amount, addEditMesh, collapseNode, centerPivot)
-    print("[ExplodeGeometry] [Poly] Finished exploding '{}'".format(node.name))
+    logger.debug("[ExplodeGeometry] [Poly] Finished exploding '%s'", node.name)
 
 
 def applySettings(n, addShell, shell_amount, addEditMesh, collapseNode, centerPivot):
-    print("[ExplodeGeometry] applySettings on '{}': shell={} (amt={}) editMesh={} collapse={} centerPivot={}".format(
-        n.name, addShell, shell_amount, addEditMesh, collapseNode, centerPivot))
+    logger.debug("[ExplodeGeometry] applySettings on '%s': shell=%s (amt=%s) editMesh=%s collapse=%s centerPivot=%s",
+        n.name, addShell, shell_amount, addEditMesh, collapseNode, centerPivot)
     if addShell:
-        print("[ExplodeGeometry] Adding Shell modifier (outerAmount={})".format(shell_amount))
+        logger.debug("[ExplodeGeometry] Adding Shell modifier (outerAmount=%s)", shell_amount)
         mod = rt.Shell()
         mod.outerAmount = shell_amount
         rt.addModifier(n, mod)
 
     if addEditMesh:
-        print("[ExplodeGeometry] Adding Edit Mesh modifier")
+        logger.debug("[ExplodeGeometry] Adding Edit Mesh modifier")
         mod = rt.Edit_Mesh()
         rt.addModifier(n, mod)
 
     if collapseNode:
-        print("[ExplodeGeometry] Collapsing modifier stack")
+        logger.debug("[ExplodeGeometry] Collapsing modifier stack")
         rt.maxOps.collapseNode(n, True)
 
     if centerPivot:
-        print("[ExplodeGeometry] Centering pivot")
+        logger.debug("[ExplodeGeometry] Centering pivot")
         rt.centerPivot(n)
 
 
